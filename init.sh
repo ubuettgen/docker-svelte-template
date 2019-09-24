f3da43250e4c94c89053f64d15a16238a3fff7ca
Mihai MICULESCU <mihaimiculescu@yahoo.com>
2019-09-24 22:40:06 +0300

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#start your new hugo&svelte project witn this
+
+cd /src/sveltedev
+npm install --force
+copy public src /src/svelte
+npm run dev
+
+#npm install creates this:
+#	node_modules
+#package-lock.json
+#package.json
+#	public
+#rollup.config.js
+#	src
+#and in the public folder:
+#favicon.png
+#global.css
+#index.html
+# npm run dev creates some extra files in public folder:
+# global.css
+# bundle.css
+# bundle.css.map
+# bundle.js
+# bundle.js.map
