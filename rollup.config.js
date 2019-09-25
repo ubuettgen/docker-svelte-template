8b7f637fdd0c48bb7f2547dc265386d8be7f9fab
Mihai MICULESCU <mihaimiculescu@yahoo.com>
2019-09-25 06:16:14 +0300

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ import { terser } from 'rollup-plugin-terser';
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-	input: '../src/svelte/src/main.js',
+	input: 'src/main.js',
 	output: {
 		sourcemap: true,
 		format: 'iife',
