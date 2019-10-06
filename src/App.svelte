8e1f77591afb242d0ccbef66e1872c7c2c2c6898
Mihai MICULESCU <mihaimiculescu@yahoo.com>
2019-10-07 02:03:06 +0300

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -9,3 +9,7 @@
 </style>
 
 <h1>Hello {name}!</h1>
+<p> Start building your app from here. <br>
+Please refer to <a href="https://svelte.dev/tutorial/basics">the Svelte tutorial</a> for further info.
+	<strong> Good luck!</strong>
+	</p>
